d5af21b86804f206111c7c44b06098b65c23ee6a
atmiya <atmiya@HARSHIL.H>
2025-07-15 11:25:45 +0530

    fsdeff

--- index.js
+++ index.js
@@ -19,14 +19,79 @@
 // Input: 5 9
 // Output: 9
 
-const a=2
-const b=3
-if(a>b)
-{
-    console.log("a is big")
+// const a=2
+// const b=3
+// if(a>b)
+// {
+//     console.log("a is big")
 
-}else
-{
-    console.log("b is big")
-}
+// }else
+// {
+//     console.log("b is big")
+// }
+
+// 3. Check Leap Year
+// Definition: Check if a given year is a leap year.
+// Input: 2020
+// Output: Leap Year
+
+// const a=2020
+// if
+
+// 4. Sum of Natural Numbers
+// Definition: Calculate the sum of the first n natural numbers.
+// Input: 5
+// Output: 15
+
+// 5. Factorial of a Number
+// Definition: Calculate factorial of a number.
+// Input: 4
+// Output: 24
+
+// 6. Print Multiplication Table
+// Definition: Display multiplication table of a number up to 10.
+// Input: 3
+// Output: 3 x 1 = 3
+// ...
+// 3 x 10 = 30
+
+
+// const num=3
+// let i=1
+// let ans=1
+// for (i=1;i<=10;i++)
+// {
+//     ans=num * i
+//     console.log(`${num} * ${i} = ${ans}`)
+// }
+
+
+
+// 7. Reverse a Number
+// Definition: Reverse the digits of a number.
+// Input: 1234
+// Output: 4321
+
+ const num=1234
+ let i=num
+ for(i=num;i>=i;i--)
+ {
+    console.log(i)
+ }
+
+
+// 8. Palindrome Check (Number)
+// Definition: Check if a number reads the same backward.
+// Input: 121
+// Output: Palindrome
+
+// 9. Check Prime Number
+// Definition: Check whether the input number is prime.
+// Input: 11
+// Output: Prime
+
+// 10. Count Digits in a Number
+// Definition: Count how many digits a number contains.
+// Input: 5023
+// Output: 4
 
