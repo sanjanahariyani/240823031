e392b8805e8ae752ec6d8fc9db4bae2ef5a03bfe
atmiya <atmiya@HARSHIL.H>
2025-07-15 10:49:50 +0530

    progr

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// 1. Check Even or Odd
+// Definition: Determine if a number is even or odd.
+// Input: 7
+// Output: Odd
+
+// const n=2
+// if(n%2==0)
+// {
+//     console.log("even number")
+
+// }else
+// {
+//     console.log("odd number")
+// }
+
+// 2. Find the Maximum of Two Numbers
+// Definition: Given two numbers, print the larger one.
+// Input: 5 9
+// Output: 9
+
+const a=2
+const b=3
+if(a>b)
+{
+    console.log("a is big")
+
+}else
+{
+    console.log("b is big")
+}
+
